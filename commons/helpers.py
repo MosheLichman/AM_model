3c1afdce7764553f97353f2e6f167662fa9e6b31
Moshe Lichman <lichman@gmail.com>
2016-10-11 16:38:20 -0700

    Adding a unique_two_cols method

--- commons/helpers.py
+++ commons/helpers.py
@@ -3,6 +3,33 @@ Author: Moshe Lichman
 """
 from __future__ import division
 from multiprocessing import Process, Queue
+import numpy as np
+
+
+def unique_two_cols(col_1, col_2):
+    """
+    Finding a unique instances of pairs. Basically it just creates a coo representation of the data.
+
+     INPUT:
+    -------
+        1. col_1:       <(N, ) ndarray>    identifier 1
+        2. col_2:       <(N, ) ndarray>    identifier 2
+
+     OUTPUT:
+    --------
+        1. coo:     <(N', 3) ndarray>       coo representation [ident_1, ident_2, counts]
+    """
+
+    if col_1.shape[0] != col_2.shape[0]:
+        raise AssertionError('Two columns are not the same length.')
+
+    x, y = col_1, col_2
+    b = x + y * 1.0j
+    idx, counts = np.unique(b, return_index=True, return_counts=True)[1:]
+
+    tmp = np.vstack([col_1[idx], col_2[idx], counts.astype(float)]).T
+
+    return tmp
 
 
 def quque_on_uids(num_proc, uids, batch_size, target, args, collect_func):
