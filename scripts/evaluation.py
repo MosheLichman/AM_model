b402b1a7d309935fab854380f4851c369ca764ae
Dimitrios Kotzias-imac <jimkotz2@gmail.com>
2016-11-22 12:03:55 -0800

    help strings, loading with numpy

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -22,7 +22,7 @@ def main_func():
     parser.add_argument('-train', type=str, help='Path to training matrix.')
     parser.add_argument('-val', type=str, help='Path to validation matrix.')
     parser.add_argument('-test', type=str, help='Path to validation matrix.')
-    parser.add_argument('-obj', type=str, help='Path to validation matrix.')
+    parser.add_argument('-obj', type=str, help='Evaluation metric string. logP or erank')
     parser.add_argument('-params', type=str, help='Path to learned params')
     parser.add_argument('-row_smooth', type=str, help='Path to row smoothing matrix.')
     parser.add_argument('-col_smooth', type=str, help='Path to column smoothing matrix.')
@@ -44,10 +44,10 @@ def main_func():
     tm.reset_tm()
 
     R, C = args.r, args.c
-    obs_mat_raw = fu.pkl_load(args.train)
+    obs_mat_raw = fu.np_load(args.train)
 
     # Combining the validation. I can treat it as train now.
-    obs_mat_val_raw = fu.pkl_load(args.val)
+    obs_mat_val_raw = fu.np_load(args.val)
     obs_mat_raw = np.vstack([obs_mat_raw, obs_mat_val_raw])
 
     train_data = sparse.coo_matrix((obs_mat_raw[:, 2], (obs_mat_raw[:, 0], obs_mat_raw[:, 1])),
@@ -55,7 +55,7 @@ def main_func():
 
     # Converting the validation data to [row_id, col_id] X number of obs. It's not as efficient but it's
     # much easier to work with that data.
-    test_mat_raw = fu.pkl_load(args.test)
+    test_mat_raw = fu.np_load(args.test)
     test_data = np.repeat(test_mat_raw[:, :-1], test_mat_raw[:, -1].astype(int), axis=0)
 
     model = am_factory.get_model(args)
@@ -67,6 +67,41 @@ def main_func():
     return results
 
 
+def get_script_str(row='U', col='S', implicit_similarity=False, min_subscribers=1000, min_posts=1000,
+                   max_words=25000, sample=False, min_col_sim=0.6, min_row_sim=0.8, rows=113557, cols=21386):
+    input_dir = '/extra/dkotzias0/reddit_data/data/input/'
+    run_name = '_%d_%d_%d' % (min_subscribers, min_posts, max_words)
+    name = '%sx%s_' % (row, col)
+
+    train = '%s%sdata%s.npy' % (input_dir, name, run_name)
+    validation = '%s%sdata%s_validation.npy' % (input_dir, name, run_name)
+    test = '%s%sdata%s_test.npy' % (input_dir, name, run_name)
+
+    'category_similarity_data_0.600_1000_1000_25000.npy'
+    if row == 'U':
+        row_sim_name = 'user'
+    elif row == 'S':
+        row_sim_name = 'category'
+    else:
+        print 'Error i dont recognize row name %s' % row
+        return
+
+    if col == 'S':
+        col_sim_name = 'category'
+    elif col == 'W':
+        col_sim_name = 'word'
+    else:
+        print 'Error i dont recognize column name %s' % col
+        return
+
+    implicit_str = ''
+    if implicit_similarity:
+        implicit_str = '_implicit'
+
+    row_sim = '%s%s_similarity%s_data_%.3f_%s.npy' % (input_dir, row_sim_name, implicit_str, min_row_sim, name)
+
+    col_sim = '%s%s_similarity%s_data_%.3f_%s.npy' % (input_dir, col_sim_name, implicit_str, min_col_sim, name)
+
+
 if __name__ == '__main__':
     res = main_func()
-
