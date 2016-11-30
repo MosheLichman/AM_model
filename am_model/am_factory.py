e00f98bc95941dc393353bce95520db535a5c7ba
Moshe Lichman <lichman@gmail.com>
2016-11-29 17:51:27 -0800

    Adding the global 3 components to the factory

--- am_model/am_factory.py
+++ am_model/am_factory.py
@@ -42,4 +42,10 @@ def _global_ar_4(args):
     return am_global.AMGlobal(args.r, args.c, 4, args.num_proc, user_smooth, item_smooth)
 
 
-_factory = {'iar2': _indiv_ar_2, 'gar2': _global_ar_2, 'iar4': _indiv_ar_4, 'gar4': _global_ar_4}
+def _global_ar_3(args):
+    item_smooth = _load_smooth_mat(args.col_smooth, args.c).tocsr()
+
+    return am_global.AMGlobal(args.r, args.c, 3, args.num_proc, None, item_smooth)
+
+
+_factory = {'iar2': _indiv_ar_2, 'gar2': _global_ar_2, 'iar4': _indiv_ar_4, 'gar4': _global_ar_4, 'gar3': _global_ar_3}
